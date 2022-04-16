ad96bc5496100346a3fd8653293396f5d5b14f33
natefoust <ogievich0@gmail.com>
2022-04-17 00:21:09 +0300

    update resources

--- oeav/Resource.h
+++ oeav/Resource.h
@@ -2,12 +2,17 @@
 // Включаемый файл, созданный в Microsoft Visual C++.
 // Используется oeav.rc
 //
+#define IDD_REGIST_LIST                 9
 #define IDD_OEAV_DIALOG                 102
 #define IDR_MAINFRAME                   128
 #define IDC_STATIC_MAIN_MENU            1000
 #define IDC_STATIC_ID                   1001
 #define IDC_STATIC_NAME                 1002
-#define IDC_SCROLLBAR1                  1003
+#define IDC_TAB_CONTROLLER              1005
+#define IDC_BUTTON_SETTINGS             1006
+#define IDC_BUTTON_EXIT                 1007
+#define IDR_SETTINGS                    1008
+#define IDR_EXIT                        1009
 
 // Next default values for new objects
 // 
@@ -15,7 +20,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
