62399f0024bdfba60462b840bd3aff6d4ca5c1ef
natefoust <ogievich0@gmail.com>
2022-04-16 19:19:45 +0300

    Extended CStatic

---
+++ oeav/controls/CStaticX.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+// CTransparentStatic
+
+class CStaticX : public CStatic
+{
+	DECLARE_DYNAMIC(CStaticX)
+
+public:
+	CStaticX();
+	virtual ~CStaticX();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnPaint();
+};
+
+
